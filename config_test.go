cf4705b8fbef60f56c3c8e7cc31cb31395b5bcf2
Ian Spence <ian@ecnepsnai.com>
2025-04-05 10:34:25 -0700

    Fix failing unit test

--- config_test.go
+++ config_test.go
@@ -29,12 +29,9 @@ func TestParseConfig(t *testing.T) {
 	dir := t.TempDir()
 	configPath := path.Join(dir, "dnsproxy.conf")
 
-	os.WriteFile(path.Join(dir, "server.crt"), []byte("1"), 0644)
-	os.WriteFile(path.Join(dir, "server.key"), []byte("1"), 0644)
-
 	config := DefaultConfig
-	config = strings.ReplaceAll(config, "/etc/dnsproxy/server.crt", path.Join(dir, "server.crt"))
-	config = strings.ReplaceAll(config, "/etc/dnsproxy/server.key", path.Join(dir, "server.key"))
+	config = strings.ReplaceAll(config, "/etc/dnsproxy/server.crt", path.Join(dir, "localhost.crt"))
+	config = strings.ReplaceAll(config, "/etc/dnsproxy/server.key", path.Join(dir, "localhost.key"))
 
 	os.WriteFile(configPath, []byte(config), 0644)
 
