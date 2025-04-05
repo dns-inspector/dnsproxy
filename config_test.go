e617e05f75e87ac888a866fa157f4989958c557d
Ian Spence <ian@ecnepsnai.com>
2025-04-05 10:40:12 -0700

    Just generate a cert instead

--- config_test.go
+++ config_test.go
@@ -19,15 +19,26 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package dnsproxy
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
 	"os"
 	"path"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestParseConfig(t *testing.T) {
 	dir := t.TempDir()
 	configPath := path.Join(dir, "dnsproxy.conf")
+
+	generateTestCert(path.Join(dir, "localhost.crt"), path.Join(dir, "localhost.key"))
 
 	config := DefaultConfig
 	config = strings.ReplaceAll(config, "/etc/dnsproxy/server.crt", path.Join(dir, "localhost.crt"))
@@ -36,4 +47,49 @@ func TestParseConfig(t *testing.T) {
 	os.WriteFile(configPath, []byte(config), 0644)
 
 	TestConfig(configPath)
+}
+
+func generateTestCert(certPath, keyPath string) {
+	pKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	pub := &pKey.PublicKey
+	serial := big.NewInt(1)
+
+	privateKeyBytes, err := x509.MarshalECPrivateKey(pKey)
+	if err != nil {
+		panic(err)
+	}
+
+	tpl := &x509.Certificate{
+		SerialNumber:          serial,
+		Subject:               pkix.Name{CommonName: "localhost"},
+		Issuer:                pkix.Name{CommonName: "localhost"},
+		NotBefore:             time.Now().UTC().AddDate(0, 0, -1),
+		NotAfter:              time.Now().UTC().AddDate(1, 0, 0),
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"localhost"},
+		IPAddresses: []net.IP{
+			net.IPv4(127, 0, 0, 1),
+			net.IPv6loopback,
+		},
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, tpl, tpl, pub, pKey)
+	if err != nil {
+		panic(err)
+	}
+
+	os.WriteFile(certPath, pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	}), 0644)
+	os.WriteFile(keyPath, pem.EncodeToMemory(&pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	}), 0644)
 }
